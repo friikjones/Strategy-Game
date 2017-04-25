19d379fc1187b25a9c2cf8a4371d2a5f0b02ca24
friikjones <d.mullerjones@gmail.com>
2017-04-25 20:37:46 -0300

    Initial Commit

---
+++ 0 - Strategy Game/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+	
+	public int cameraAngle;
+	public int cameraZoom;
+	
+	private int cameraRadius;
+	private int cameraFieldofView;
+	private int cameraZoomCap;
+	private int cameraZoomFieldVariable;
+	private int cameraZoomFieldBase;
+	private int cameraPanVariable;
+	private float centerX;
+	private float calculatedX;
+	private float centerZ;
+	private float calculatedZ;
+	
+	private KeyCode zoomIn;
+	private KeyCode zoomOut;
+	private KeyCode rotateLeft;
+	private KeyCode rotateRight;
+	private KeyCode moveLeft;
+	private KeyCode moveRight;
+	private KeyCode moveFront;
+	private KeyCode moveBack;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		cameraAngle = 0;
+		cameraRadius = 10;
+		cameraZoomCap = 2;
+		cameraZoom = 1;
+		
+		cameraZoomFieldBase = 30;
+		cameraZoomFieldVariable = 30;
+		cameraPanVariable = 2;
+		
+		
+		zoomIn = KeyCode.R;
+		zoomOut = KeyCode.F;
+		rotateLeft = KeyCode.Q;
+		rotateRight = KeyCode.E;
+		moveLeft = KeyCode.A;
+		moveRight = KeyCode.D;
+		moveFront = KeyCode.W;
+		moveBack = KeyCode.S;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		cameraRefresh();
+		cameraInput();
+	}
+	
+	void cameraRefresh()
+	{
+		calculatedX = Mathf.Sin(cameraAngle * Mathf.PI/180) * cameraRadius * -1 + centerX;
+		calculatedZ = Mathf.Cos(cameraAngle * Mathf.PI/180) * cameraRadius * -1 + centerZ;
+		
+		transform.position = new Vector3(calculatedX, 10, calculatedZ);
+		transform.eulerAngles = new Vector3(45, cameraAngle, 0);
+		
+		Camera.main.fieldOfView = (cameraZoom * cameraZoomFieldVariable) + cameraZoomFieldBase;
+	}
+	
+	void cameraInput()
+	{
+		if (Input.GetKeyDown(zoomOut))
+		{
+			if (cameraZoom < cameraZoomCap)
+			cameraZoom++;
+		}
+		
+		if (Input.GetKeyDown(zoomIn))
+		{
+			if (cameraZoom > 0)
+			cameraZoom--;
+		}
+		
+		if (Input.GetKeyDown(rotateLeft))
+		{
+			cameraAngle = cameraAngle + 45;
+		}
+		
+		if (Input.GetKeyDown(rotateRight))
+		{
+			cameraAngle = cameraAngle - 45;
+		}
+		
+		if (Input.GetKeyDown(moveRight))
+		{
+			centerX = centerX + Mathf.Cos(cameraAngle * Mathf.PI/180) * cameraPanVariable;
+			centerZ = centerZ - Mathf.Sin(cameraAngle * Mathf.PI/180) * cameraPanVariable;
+		}
+		
+		if (Input.GetKeyDown(moveLeft))
+		{
+			centerX = centerX - Mathf.Cos(cameraAngle * Mathf.PI/180) * cameraPanVariable;
+			centerZ = centerZ + Mathf.Sin(cameraAngle * Mathf.PI/180) * cameraPanVariable;
+		}
+		
+		if (Input.GetKeyDown(moveFront))
+		{
+			centerX = centerX + Mathf.Sin(cameraAngle * Mathf.PI/180) * cameraPanVariable;
+			centerZ = centerZ + Mathf.Cos(cameraAngle * Mathf.PI/180) * cameraPanVariable;
+		}
+		
+		if (Input.GetKeyDown(moveBack))
+		{
+			centerX = centerX - Mathf.Sin(cameraAngle * Mathf.PI/180) * cameraPanVariable;
+			centerZ = centerZ - Mathf.Cos(cameraAngle * Mathf.PI/180) * cameraPanVariable;
+		}
+		
+	}
+}
